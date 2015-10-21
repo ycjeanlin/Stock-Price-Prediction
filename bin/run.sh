9e75ce49591af40d6f206ceadb6303759a16c95d
Jean Lin <yzl0083@outlook.com>
2015-10-21 17:03:16 +0800

    Prepare for ex3

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,13 @@
-python prepare-data.py 2015-07-01 2015-07-31 5 ../ex2/train-data
-python prepare-data.py 2015-08-01 2015-08-31 5 ../ex2/test-data
+# prepare data
+python prepare-data.py 2015-07-01 2015-07-31 5 ../ex3/train-data
+python prepare-data.py 2015-08-01 2015-08-31 5 ../ex3/test-data
+
+# Build model
+cd ../ex3
+vm-scale -s train-data-scale-info train-data > train-data.scale
+svm-train -b 1 train-data.scale model/train-model
+
+# Test model
+svm-scale -r train-data-scale-info test-data > test-data.scale
+svm-predict -b 1 test-data.scale model/train-model test-result
+
