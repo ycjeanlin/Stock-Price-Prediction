1a6e8c6aa52651ea1ddbf8da5b88f082b7f19b4f
Jean Lin <yzl0083@outlook.com>
2015-10-28 22:23:09 +0800

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -11,11 +11,11 @@
 #svm-predict -b 1 test-data.scale model/train-model test-result
 
 # Generate the predicting data
-#python ./generate-predict-data.py $1 5 ../output/predict-data ../output/predict-stock-list
+python ./generate-predict-data.py $1 5 ../output/predict-data ../output/predict-stock-list
 
 # Predict
-#svm-scale -r model/train-data-scale-info ../output/predict-data > ../output/predict-data.scale
-#svm-predict -b 1 ../output/predict-data.scale model/train-model ../output/predict-result
+svm-scale -r model/train-data-scale-info ../output/predict-data > ../output/predict-data.scale
+svm-predict -b 1 ../output/predict-data.scale model/train-model ../output/predict-result
 
 # Make decision
 python ./make-decision.py ../output/predict-result ../output/predict-stock-list ../commit/$1.json
