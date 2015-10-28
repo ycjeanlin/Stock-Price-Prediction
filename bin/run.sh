3ddccc207701a13c002513db3ff25b48eb16d509
Jean Lin <yzl0083@outlook.com>
2015-10-28 21:38:27 +0800

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,10 @@
 # prepare data
-#python prepare-data.py 2015-06-01 2015-08-31 5 ../output/train-data
+python prepare-data.py 2015-09-01 2015-09-30 5 ../output/train-data
 #python prepare-data.py 2015-08-01 2015-08-31 5 ../output/test-data
 
 # Build model
-#svm-scale -s ./model/train-data-scale-info ../output/train-data > ../output/train-data.scale
-#svm-train -b 1 ../output/train-data.scale ./model/train-model
+svm-scale -s ./model/train-data-scale-info ../output/train-data > ../output/train-data.scale
+svm-train -b 1 ../output/train-data.scale ./model/train-model
 
 # Test model
 #svm-scale -r train-data-scale-info test-data > test-data.scale
@@ -18,5 +18,5 @@ svm-scale -r model/train-data-scale-info ../output/predict-data > ../output/pred
 svm-predict -b 1 ../output/predict-data.scale model/train-model ../output/predict-result
 
 # Make decision
-python ./make-decision.py ../output/predict-result ../output/predict-stock-list ../commit/$1.json
+#python ./make-decision.py ../output/predict-result ../output/predict-stock-list ../commit/$1.json
 
